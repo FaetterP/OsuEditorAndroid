82bb870efc83bd03ad30fcd1b19eefe2caf1c9ef
FaetterP <56697273+FaetterP@users.noreply.github.com>
2021-08-09 02:52:39 +0300

    Изменено название поля в PrinterNumbers

--- Assets/Utilities/PrinterNumber.cs
+++ Assets/Utilities/PrinterNumber.cs
@@ -5,14 +5,14 @@ namespace Assets.Utilities
 {
     class PrinterNumber : MonoBehaviour
     {
-        private GameObject[] Numbers = new GameObject[10];
+        private GameObject[] numbers = new GameObject[10];
         public int number;
 
         void Awake()
         {
             for(int i = 0; i <= 9; i++)
             {
-                Numbers[i] = Resources.Load("Number" + i) as GameObject;
+                numbers[i] = Resources.Load("Number" + i) as GameObject;
             }
         }
 
@@ -21,10 +21,10 @@ namespace Assets.Utilities
             int offset = 0;
             for (int i = 0; i <= (int)Math.Log10(number); i++)
             {
-                GameObject created = Instantiate(Numbers[number % 10], transform);
+                GameObject created = Instantiate(numbers[number % 10], transform);
                 created.transform.localPosition = new Vector2(offset, 0);
                 offset -= 17;
-                number = number / 10;
+                number /= 10;
             }
         }
     }
