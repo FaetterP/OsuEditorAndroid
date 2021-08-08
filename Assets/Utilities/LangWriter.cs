ebb6aa24e6469755cac9f29b8ccf94a081dd5af1
FaetterP <56697273+FaetterP@users.noreply.github.com>
2021-08-09 02:11:42 +0300

    Убран switch из LangWriter
    Добавлен словарь <Lang, string>, который удобно расширять и по которому осуществляется извлечение строки.

--- Assets/Utilities/LangWriter.cs
+++ Assets/Utilities/LangWriter.cs
@@ -1,40 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Assets.Utilities
 {
+    [RequireComponent(typeof(Text))]
     class LangWriter : MonoBehaviour
     {
-        [SerializeField] [TextArea()] private string en = "", ru = "";
+        [SerializeField] [TextArea()] private string en, ru;
+        private Dictionary<Lang, string> localizedText;
+        private Text thisText;
+
+        void Awake()
+        {
+            thisText = GetComponent<Text>();
+
+            localizedText = new Dictionary<Lang, string>();
+            localizedText.Add(Lang.EN, en);
+            localizedText.Add(Lang.RU, ru);
+        }
 
         void OnEnable()
         {
-            Text TextOnThisObj = GetComponent<Text>();
-            switch (Global.Lang)
-            {
-                case Lang.EN:
-                    TextOnThisObj.text = en;
-                    break;
-
-                case Lang.RU:
-                    TextOnThisObj.text = ru;
-                    break;
-            }
+            thisText.text = localizedText[Global.Lang];
         }
 
         public string GetText()
         {
-            switch (Global.Lang)
-            {
-                case Lang.EN:
-                    return(en);
-                case Lang.RU:
-                    return(ru);
-            }return null;
+            return localizedText[Global.Lang];
         }
     }
 }
