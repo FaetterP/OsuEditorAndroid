7b1d136faf646863f57ef9440df0fd094d80d04a
FaetterP <faettterp@gmail.com>
2021-08-17 13:53:54 +0300

    Обновлён OpenerUrl
    Добавлен словарь для доступа к ссылкам, в зависимости от текущего языка.
    Предыдущий подход был очень негибкий.

--- Assets/Utilities/OpenerUrl.cs
+++ Assets/Utilities/OpenerUrl.cs
@@ -1,17 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Utilities
 {
     class OpenerUrl : MonoBehaviour
     {
-        [SerializeField] private string URL_1 = "", URL_2 = "";
+        [SerializeField] private string url_EN, url_RU;
+        private Dictionary<Lang, string> urls;
+
+        void Awake()
+        {
+            urls = new Dictionary<Lang, string>();
+            urls.Add(Lang.EN, url_EN);
+            urls.Add(Lang.RU, url_RU);
+        }
+
         void OnMouseDown()
         {
-            Application.OpenURL(URL_1 + Global.Lang + URL_2);
+            Application.OpenURL(urls[Global.Lang]);
         }
     }
 }
