0b7aa3648d0b541324a7026d1f9da76f556ff2b2
FaetterP <faettterp@gmail.com>
2021-08-19 23:13:45 +0300

    Переименованы некоторые классы

---
+++ Assets/Music/MusicVolumeSlider.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Music
+{
+    [RequireComponent(typeof(Slider))]
+    class MusicVolumeSlider : MonoBehaviour
+    {
+        [SerializeField] private AudioSource _music;
+                         private Slider _thisSlider;
+        
+        void Awake()
+        {
+            _thisSlider = GetComponent<Slider>();
+            _thisSlider.onValueChanged.AddListener(delegate { UpdateVolume(); });
+        }
+
+        private void UpdateVolume()
+        {
+            _music.volume = _thisSlider.value;
+        }
+    }
+}
