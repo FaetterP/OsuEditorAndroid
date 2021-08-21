9f7b07a24d6e5093d5617a268b93a59b30ee17fb
FaetterP <faettterp@gmail.com>
2021-08-21 13:08:26 +0300

    Почищен Preproducer

--- Assets/OsuEditor/Preproducer.cs
+++ Assets/OsuEditor/Preproducer.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using Assets.CreateLoad;
 using Assets.Music;
 using System.Collections;
-using UnityEngine.Networking;
 using Assets.OsuEditor;
 using Assets.OsuEditor.Timeline;
 
@@ -15,71 +10,48 @@ namespace Assets.Editor
 {
     class Preproducer : MonoBehaviour
     {
-        [SerializeField] private Image background;
-        [SerializeField] private AudioSource music;
-        [SerializeField] private TimeLine line;
-        [SerializeField] private CreatorMusicLineMarks creator;
-        [SerializeField] private TimelineStepSlider timelineSlider;
-        private WWW wwwBackground, wwwMusic;
+        [SerializeField] private Image _background;
+        [SerializeField] private AudioSource _music;
+        [SerializeField] private TimeLine _line;
+        [SerializeField] private CreatorMusicLineMarks _creator;
+        [SerializeField] private TimelineStepSlider _timelineSlider;
+                         private WWW _wwwBackground;
 
         void Awake()
         {
-            QualitySettings.SetQualityLevel(5, true);
             Reader.LoadMapFromFile(Global.FullPathToMap);
 
-            wwwBackground = new WWW("file:///" + Global.FullPathToMapFolder + Global.Map.Events.BackgroungImage);
-            //wwwMusic = new WWW("file:///" + Global.FullPathToMapFolder + Global.Map.General.AudioFilename);
+            _wwwBackground = new WWW("file:///" + Global.FullPathToMapFolder + Global.Map.Events.BackgroungImage);
         }
         void Start()
         {
             SetBackground();
             SetMusic();
-
-            //  Destroy(gameObject);
         }
 
 
         private void SetBackground()
         {
-            while (!wwwBackground.isDone) { }
-            background.sprite = Sprite.Create(wwwBackground.texture, new Rect(0, 0, wwwBackground.texture.width, wwwBackground.texture.height), new Vector2(0, 0));
+            Rect textureRect = new Rect(0, 0, _wwwBackground.texture.width, _wwwBackground.texture.height);
+            _background.sprite = Sprite.Create(_wwwBackground.texture, textureRect , Vector2.zero);
         }
-
-        //private void SetMusic()
-        //{
-        //    while (!wwwMusic.isDone) {  }
-        //    AudioClip clip = NAudioPlayer.FromMp3Data(wwwMusic.bytes);
-        //    clip.name = "audio_from_file";
-        //    music.clip = clip;
-        //    line.UpdateMax((int)(clip.length*1000));
-        //}
 
         public void SetMusic()
         {
            StartCoroutine(LoadSongCoroutine());
         }
 
-        //IEnumerator LoadSongCoroutine()
-        //{
-        //    string url = "file:///" + Global.FullPathToMapFolder + Global.Map.General.AudioFilename;
-        //    WWW www = new WWW(url);
-        //    yield return www;
-
-        //    music.clip = www.GetAudioClip(false, false);
-        //    line.UpdateMax((int)(music.clip.length * 1000));
-        //    yield break;
-        //}
         IEnumerator LoadSongCoroutine()
         {
             string url = "file:///" + Global.FullPathToMapFolder + Global.Map.General.AudioFilename;
-            WWW www = new WWW(url);
-            yield return www;
+            WWW wwwMusic = new WWW(url);
+            yield return wwwMusic;
 
-            music.clip = www.GetAudioClip(false, false);
-            Global.MusicLength = (int)(music.clip.length * 1000);
-            line.UpdateMax((int)(music.clip.length * 1000));
-            creator.UpdateMarks();
-            timelineSlider.UpdateMarks();
+            _music.clip = wwwMusic.GetAudioClip(false, false);
+            Global.MusicLength = (int)(_music.clip.length * 1000);
+            _line.UpdateMax((int)(_music.clip.length * 1000));
+            _creator.UpdateMarks();
+            _timelineSlider.UpdateMarks();
             yield break;
         }
     }
